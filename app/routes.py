23e820b9a9c54047afcf5710893a3af1dc012350
sunchengyi <ddsunchengyi@gmail.com>
2021-04-18 22:56:01 +0900

    Use 'url_for()' to replace the direct url in func 'login' and 'base.html'.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 '''
 Home page route
 '''
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.form import LoginForm
 
@@ -29,5 +29,5 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
