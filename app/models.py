39360a32228fa29a4248189e7ff8340440866898
sunchengyi <ddsunchengyi@gmail.com>
2021-05-02 11:17:17 +0900

    Add a method in User to get the related posts (followed users and owns).

--- app/models.py
+++ app/models.py
@@ -30,12 +30,33 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+    
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id==user.id).count()>0
+
+    def followed_posts(self):
+        followed = Post.query.join(followers, (followers.c.followed_id==Post.user_id)).filter(
+            followers.c.follower_id==self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).sort_by(Post.timestamp.desc())
+        
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
