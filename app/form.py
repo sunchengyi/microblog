1e4f595782b3488dab015cc725d6587ef56db53d
sunchengyi <ddsunchengyi@gmail.com>
2021-05-02 21:40:54 +0900

    Correct some errors.

--- app/form.py
+++ app/form.py
@@ -50,5 +50,5 @@ class EmptyForm(FlaskForm):
 
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
-        DataRequired, Length(min=1, max=140)])
+        DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 from flask import render_template, flash, redirect, url_for
 from app import app, db
-from app.form import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.form import LoginForm, RegistrationForm, EditProfileForm, EmptyForm,\
+    PostForm
 
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import request
 from werkzeug.urls import url_parse
-from app.models import User
+from app.models import User, Post
  
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
