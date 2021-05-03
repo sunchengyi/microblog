8e344d9c21b8f01ed219dd60c99b9c648d898d72
sunchengyi <ddsunchengyi@gmail.com>
2021-05-03 11:28:41 +0900

    Complete chapter 9 by adding pagination in profile page.

--- app/routes.py
+++ app/routes.py
@@ -92,12 +92,16 @@ def user(username):
     The profile view of user.
     """
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form1=form)
+    return render_template('user.html', user=user, posts=posts.items, 
+                           next_url=next_url, prev_url=prev_url, form1=form)
 
 @app.before_request
 def before_request():
