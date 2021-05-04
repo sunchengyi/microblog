010d091e41a6ebde0acaeefc302d781626887843
sunchengyi <ddsunchengyi@gmail.com>
2021-05-05 00:04:21 +0900

    Complete chapter 12.

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,14 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,6 +23,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 if not app.debug:
     # the emailing error handler
