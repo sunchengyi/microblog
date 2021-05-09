07bf3fa49c2b40341f61ad8dff34dab2494171b0
sunchengyi <ddsunchengyi@gmail.com>
2021-05-09 17:20:37 +0900

    Modify 'before_request()' to set the language in 'moment.js'.

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,11 @@ Home page route
 '''
 from datetime import datetime
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, g
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import request
 from werkzeug.urls import url_parse
-from flask_babel import _
+from flask_babel import _, get_locale
 
 from app import app, db
 from app.form import LoginForm, RegistrationForm, EditProfileForm, EmptyForm,\
@@ -20,6 +20,14 @@ def myurl():
     def myurl_for(s):
         return url_for(s)
     return dict(myurl_for=myurl_for)
+
+@app.before_request
+def before_request():
+    g.locale = str(get_locale())
+    if g.locale == 'zh': g.locale = 'zh-cn'
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -110,12 +118,6 @@ def user(username):
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts.items, 
                            next_url=next_url, prev_url=prev_url, form1=form)
-
-@app.before_request
-def before_request():
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
