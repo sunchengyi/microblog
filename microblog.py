11665fb22fd2a997bb7d2ce75c4f157f3ab3f4aa
sunchengyi <ddsunchengyi@gmail.com>
2021-05-19 23:11:38 +0900

    Complete the Application Factory Pattern. The testing part is not modified.

--- microblog.py
+++ microblog.py
@@ -1,6 +1,9 @@
-from app import app, db, cli
+from app import create_app, db, cli
 from app.models import User, Post
 from app.translate import translate
+
+app = create_app()
+cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
