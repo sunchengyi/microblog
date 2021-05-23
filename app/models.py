524270f539f0b63899e7d7e05098e0c4984f0dcd
sunchengyi <ddsunchengyi@gmail.com>
2021-05-23 13:27:00 +0900

    Define a class 'SearchableMaxin' in 'models.py' to integrate searches with SQLAlchemy.

--- app/models.py
+++ app/models.py
@@ -8,6 +8,47 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 
 from app import db, login
+from app.search import add_to_index, remove_from_index, query_index
+
+class SearchableMaxin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when =[(id, i) for i, id in enumerate(ids)]
+        #for i in range(len(ids)):
+        #    when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(
+            db.case(when, value=cls.id)), total
+    
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+    
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMaxin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMaxin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMaxin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+db.event.listen(db.session, 'before_commit', SearchableMaxin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMaxin.after_commit)
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -74,7 +115,7 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
-class Post(db.Model):
+class Post(SearchableMaxin, db.Model):
     __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
