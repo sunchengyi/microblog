879785eca8a9537a647a27a85928ca3db8a25155
sunchengyi <ddsunchengyi@gmail.com>
2021-06-07 23:11:05 +0900

    Chap21: Change DB to support the private messages.

--- app/models.py
+++ app/models.py
@@ -68,6 +68,17 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id==id),
         secondaryjoin=(followers.c.followed_id==id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    messages_sent = db.relationship('Message', foreign_keys='Message.sender_id',
+                                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message', 
+                                        foreign_keys='Message.receipient_id', 
+                                        backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900,1,1)
+        return Message.query().filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -125,6 +136,13 @@ class Post(SearchableMaxin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    boday = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 @login.user_loader
 def load_user(id):
