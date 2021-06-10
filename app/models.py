8647c08f355f5143ba10306c923d8f91501b5ae5
sunchengyi <ddsunchengyi@gmail.com>
2021-06-10 23:22:32 +0900

    Add the function viewing private messages. But something is wrong about the database.

--- app/models.py
+++ app/models.py
@@ -143,6 +143,9 @@ class Message(db.Model):
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
 
 @login.user_loader
 def load_user(id):
