5d37e93d7b374eb38180926abab43e47d89eb943
sunchengyi <ddsunchengyi@gmail.com>
2021-06-16 22:29:32 +0900

    Modify email.py

--- app/email.py
+++ app/email.py
@@ -10,12 +10,19 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(subject, sender, recipients, text_body, html_body,
+               attachments=None, sync=False):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    # here, "current_app._get_current_object()" is necessary,
-    # since "current_app" is a proxy object and only works in a thred handling 
-    # the client request.
-    Thread(target=send_async_email, 
-           args=(current_app._get_current_object(), msg)).start()
+    if attachments:
+        for attachment in attachments:
+            msg.attach(*attachment)
+    if sync:
+        mail.send(msg)
+    else:
+        # here, "current_app._get_current_object()" is necessary,
+        # since "current_app" is a proxy object and only works in a thred handling 
+        # the client request.
+        Thread(target=send_async_email, 
+            args=(current_app._get_current_object(), msg)).start()
