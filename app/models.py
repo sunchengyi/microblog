344398ced081b609faf1525dc1fa07ee968cea82
sunchengyi <ddsunchengyi@gmail.com>
2021-06-24 23:11:38 +0900

    Add a class, 'PaginateAPIMixin' in 'models.py' and add this class to be one of the parent class of 'User'. Complet the func, 'bad_request()' in 'app.appi.errors.py'.

--- app/models.py
+++ app/models.py
@@ -60,7 +60,30 @@ followers = db.Table('followers',
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(UserMixin, db.Model):
+class PaginateAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page, 
+                                **kwargs),
+                'next': url_for(endpoint, page=page+1, per_page=per_page, 
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page-1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
+class User(PaginateAPIMixin, UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
